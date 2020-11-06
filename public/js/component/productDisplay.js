b6cb1225b110db26aa16ca6ce504da420d7f9e44
CarinaHerlina <55306612+CarinaHerlina@users.noreply.github.com>
2020-11-05 23:23:40 -0700

    Submitted check-in 1
    Server is working, and I clean up the folders that I am guessing are just a reference on how we want to create our pages.

---
+++ public/js/component/productDisplay.js
@@ -0,0 +1,15 @@
+class ProductDisplay extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            appVersion: ''
+        }
+    }
+
+    render() {
+        return(
+            <div></div>
+        )
+    }
+}
