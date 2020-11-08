8dd2fdc77906f9e61969cae38ab55b301aa0ffa2
mattzamora21 <68615568+mattzamora21@users.noreply.github.com>
2020-11-07 18:37:23 -0700

    added CSE project

---
+++ CPI310#2 copy/zamora_matthew_main.js
@@ -0,0 +1,23 @@
+var modal = document.getElementById('id01');
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
