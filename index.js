b187bb70b09a38940747f56d7f79a6c91393e32f
Yash Babaria <32756653+yashbabaria@users.noreply.github.com>
2020-11-10 15:01:25 -0700

    added a bunch of stuff

--- index.js
+++ index.js
@@ -2,12 +2,65 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.use(express.static(__dirname + '/public'));
+// importing sqlite
+const sqlite3 = require('sqlite3')
+const { open } = require('sqlite')
+
+// opening sqlite db
+const dbPromise = open({
+  filename: "./Primary.sqlite",
+  driver: sqlite3.Database,
+})
+
+// import bcrypt to hash the users password
+const bcrypt = require('bcrypt')
+const saltRounds = 10
+
+app.use(express.static(__dirname + "/public"));
 
 app.get('/', (req, res) => {
     console.log(__dirname);
     res.sendFile(__dirname + '/index.html');
 });
+
+app.use(express.urlencoded());
+//Sign Up
+app.post('/register', async (req,res) => {
+  /*
+  Insert to customers Table:
+      const name = req.body.name
+      const dob = req.body.dob
+      const email = req.body.email
+  Insert to customer_private Table:
+      const address = req.body.address
+      const city = req.body.city
+      const state = req.body.state
+      const country = req.body.country
+      const postalCode = req.body.postalCode
+  */
+  const username = req.body.username
+  const password = req.body.pass
+  //hashing the password
+  const hashPassword = await bcrypt.hash(password, saltRounds)
+  if(username && password){
+    const db = await dbPromise;
+    await db.run("INSERT INTO customers(username,password) VALUES (?,?)", username, hashPassword)
+    //res.send(`Hello ${username}, With Password ${password}`)
+  }else{
+    res.sendFile('/User\ SignUp/index.html')
+  }
+})
+app.get('/register', (req, res) => {
+  res.sendFile('/User\ SignUp/index.html')
+})
+
+//Sign In
+app.post('/login', async (req,res) => {
+
+})
+app.get('/login', async (req,res) => {
+    res.sendFile('/User\ Login/index.html')
+})
 
 app.listen(port, (error) => {
     if (error)
