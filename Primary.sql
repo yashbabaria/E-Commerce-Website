a39959650cf6d43c45bbf0e1f68bdae5b31df005
Yash Babaria <32756653+yashbabaria@users.noreply.github.com>
2020-11-16 10:22:36 -0700

    Fixed Database & bodyparser

--- Primary.sql
+++ Primary.sql
@@ -10,16 +10,16 @@ CREATE TABLE IF NOT EXISTS "Order" (
 	"Order_ID"	INTEGER NOT NULL UNIQUE,
 	"Customer_ID"	INTEGER,
 	"Shipping_Date"	TEXT,
-	FOREIGN KEY("Customer_ID") REFERENCES "customers"("customer_id"),
-	PRIMARY KEY("Order_ID" AUTOINCREMENT)
+	PRIMARY KEY("Order_ID" AUTOINCREMENT),
+	FOREIGN KEY("Customer_ID") REFERENCES "customers"("customer_id")
 );
 CREATE TABLE IF NOT EXISTS "OrderDetails" (
 	"Order_ID"	INTEGER NOT NULL UNIQUE,
 	"Product_ID"	INTEGER,
 	"Quantity"	INTEGER,
+	PRIMARY KEY("Order_ID" AUTOINCREMENT),
 	FOREIGN KEY("Order_ID") REFERENCES "Order"("Order_ID"),
-	FOREIGN KEY("Product_ID") REFERENCES "Product"("Product_Id"),
-	PRIMARY KEY("Order_ID" AUTOINCREMENT)
+	FOREIGN KEY("Product_ID") REFERENCES "Product"("Product_Id")
 );
 CREATE TABLE IF NOT EXISTS "Product" (
 	"Product_Id"	INTEGER,
@@ -32,32 +32,34 @@ CREATE TABLE IF NOT EXISTS "Product" (
 	"Image"	TEXT,
 	PRIMARY KEY("Product_Id")
 );
-CREATE TABLE IF NOT EXISTS "customer_private" (
-	"customer_id"	INTEGER NOT NULL UNIQUE,
-	"Home Address"	TEXT,
-	"Billing Address"	TEXT,
-	"Apartment Address"	TEXT,
-	"City"	TEXT NOT NULL,
-	"State"	TEXT NOT NULL,
-	"Country"	TEXT NOT NULL,
-	"Postal Code"	NUMERIC NOT NULL,
-	FOREIGN KEY("customer_id") REFERENCES "customers"("customer_id"),
-	PRIMARY KEY("customer_id")
-);
 CREATE TABLE IF NOT EXISTS "Review" (
 	"Customer_Id"	NUMERIC NOT NULL,
 	"Description"	TEXT,
 	"Rating"	INTEGER,
-	FOREIGN KEY("Customer_Id") REFERENCES "customers"("customer_id"),
-	PRIMARY KEY("Customer_Id")
+	PRIMARY KEY("Customer_Id"),
+	FOREIGN KEY("Customer_Id") REFERENCES "customers"("customer_id")
 );
 CREATE TABLE IF NOT EXISTS "customers" (
 	"customer_id"	INTEGER NOT NULL UNIQUE,
-	"name"	TEXT,
-	"Date_of_Birth"	TEXT,
+	"firstname"	TEXT,
+	"lastname"	TEXT,
 	"Email"	TEXT,
 	"Username"	TEXT,
 	"Password"	TEXT,
 	PRIMARY KEY("customer_id" AUTOINCREMENT)
 );
+CREATE TABLE IF NOT EXISTS "customer_private" (
+	"customer_id"	INTEGER NOT NULL UNIQUE,
+	"Home_Address"	TEXT,
+	"Billing_Address"	TEXT,
+	"Apartment_Address"	TEXT,
+	"City"	TEXT NOT NULL,
+	"State"	TEXT NOT NULL,
+	"Country"	TEXT,
+	"Postal_Code"	INTEGER NOT NULL,
+	PRIMARY KEY("customer_id"),
+	FOREIGN KEY("customer_id") REFERENCES "customers"("customer_id")
+);
+INSERT INTO "customers" VALUES (1,'Yash','Babaria','ybabaria@asu.edu','YashMilk','super980');
+INSERT INTO "customers" VALUES (2,'Jase','Pippe','yoshieg.babaria@gmail.com','Youngin','never');
 COMMIT;
--- index.js
+++ index.js
@@ -23,40 +23,43 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-app.use(express.urlencoded());
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json());
 //Sign Up
 app.post('/register', async (req,res) => {
   const db = await dbPromise;
-  //const name = req.body.firstname + " " + req.body.lastname
-  //const dob = req.body.dob
-/*
-  const address = req.body.address
-  const city = req.body.city
-  const state = req.body.state
-  const country = req.body.country
-  const postalCode = req.body.postalCode
-  if(address & city & state & country & postalCode){
-    await db.run("INSERT INTO customer_private(Home Adress, City, State, Country, Postal Code)",
-                address, city, state, country, postalCode)
-  }*/
+
+  // const country = req.body.country
+
+
+  const firstname = req.body.firstname
+  const lastname = req.body.lastname
   const email = req.body.email
   const username = req.body.username
   const password = req.body.pass
+
   //hashing the password
-  const hashPassword = await bcrypt.hash(password, saltRounds)
-  if(username && password){
-    await db.run("INSERT INTO customers(username,password) VALUES (?,?)", username, hashPassword)
-    res.redirect("/")
+  //const hashPassword = await bcrypt.hash(password, saltRounds)
+  const userTaken = await db.run("SELECT Username FROM customers WHERE Username=?", username)
+  if(userTaken){
+    console.log("Username taken");
+  }else if(email && username && password){
+    await db.run("INSERT INTO customers(firstname,lastname,Email,Username,Password) VALUES (?,?,?,?,?)",
+            firstname,lastname,email, username, password)
+              res.redirect("/")
     //res.send(`Hello ${username}, With Password ${password}`)
   }else {
     console.log("ERROR");
   }
+
 })
 app.get('/register', (req, res) => {
   res.sendFile(__dirname + '/User\ SignUp/index.html')
 })
 
-//Sign In
+//Sign In Works
 app.post('/login', async (req,res) => {
   const db = await dbPromise
   const username = req.body.username
@@ -64,23 +67,64 @@ app.post('/login', async (req,res) => {
   const user = await db.get("SELECT * FROM customers WHERE username=?", username)
   if(!user){
      // error username not found in database
-     res.send("Username not Found")
-  }
-  const userPassword = await db.get("SELECT password FROM customers WHERE username=?", username)
-  const passwordMatches = await bcrypt.compare(password, userPassword)
-  console.log(user.password);
-  if(!passwordMatches){
-    // error password not found in database
-    res.send("The password is incorrect")
-  }
-  res.redirect("/")
+     alert("Username not Found")
+  }else{
+    if(!user.Password){
+      // error password not found in database
+      alert("The password is incorrect")
+    }else{
+      res.redirect("/")
+    }
 
+  }
+  //console.log(user.Password);
 
 })
+
 app.get('/login', async (req,res) => {
     res.sendFile(__dirname + '/User\ Login/index.html')
 })
 
+app.get('/payment', async (req,res) => {
+    // Payment page.
+})
+
+app.post('/payment', async (req,res) => {
+  // Get the cart, add the prices, when the user clicks on the pay send to summary screen then go back home.
+})
+
+app.post('/address', async (req,res) =>
+  //not working yet
+  const home_address = req.body.home_address
+  const billing_address = req.body.billing_address
+  const apart_address = req.body.apart_address
+
+  const city = req.body.city
+  const state = req.body.state
+  const postalCode = req.body.postalCode
+  const user = await db.run("SELECT * FROM customers WHERE Username=? AND Password=?", username, password)
+  if(city & state & postalCode){
+    if(home_address){
+      await db.run("INSERT INTO customer_private(customer_id,Home_Address,City,State,Postal_Code) VALUES (?,?,?,?,?)",
+                  user.customer_id, city, state, postalCode)
+      console.log("Location inputed");
+    }else if(billing_address){
+      await db.run("INSERT INTO customer_private(customer_id,Billing_Address, City,State,Postal_Code) VALUES (?,?,?,?,?)",
+                  user.customer_id, city, state, postalCode)
+      console.log("Location inputed");
+    }else if(apart_address){
+      await db.run("INSERT INTO customer_private(customer_id,Apartment_Address,City,State,Postal_Code) VALUES (?,?,?,?,?)",
+                  user.customer_id, city, state, postalCode)
+      console.log("Location inputed");
+    }else{
+      console.log("Location is not inputed");
+    }
+
+  }
+})
+app.get('/address', (req,res) => {
+  // account address input page.
+})
 app.listen(port, (error) => {
     if (error)
     {
